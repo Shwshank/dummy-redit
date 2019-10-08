60c48fc8d1c2ad36b7e3661dab12ef46f66bac69
shwshank <shwshank3@gmail.com>
2019-10-08 18:31:05 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <p> react works! </p>
+    </div>
+  );
+}
+
+export default App;
